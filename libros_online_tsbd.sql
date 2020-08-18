6cdc90309c09f456effe60313881270230f2915c
Hector JS <miguel.lin123@gmail.com>
2020-08-17 23:52:59 -0500

    generar campos nuevos y datos en la tabla de libros

--- libros_online_tsbd.sql
+++ libros_online_tsbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-08-2020 a las 04:19:10
+-- Tiempo de generación: 18-08-2020 a las 06:50:46
 -- Versión del servidor: 10.3.16-MariaDB
 -- Versión de PHP: 7.3.7
 
@@ -33,7 +33,6 @@ USE `libros_online_tsbd`;
 CREATE TABLE `carrito` (
   `id_carrito` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `costo_total` int(4) NOT NULL,
   `cantidad_total` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -48,8 +47,8 @@ CREATE TABLE `compra` (
   `nom_compra` varchar(30) COLLATE utf8_bin NOT NULL,
   `fecha_compra` date NOT NULL,
   `costo_total` int(4) NOT NULL,
-  `cantidad total` int(3) NOT NULL,
   `descripcion` text COLLATE utf8_bin NOT NULL,
+  `metodo_pago` varchar(20) COLLATE utf8_bin NOT NULL,
   `id_carrito` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -76,9 +75,17 @@ CREATE TABLE `facturas` (
   `id_facturas` int(11) NOT NULL,
   `nombre` varchar(30) COLLATE utf8_bin NOT NULL,
   `apellido` varchar(30) COLLATE utf8_bin NOT NULL,
+  `email` varchar(30) COLLATE utf8_bin NOT NULL,
+  `uso_cfdi` varchar(30) COLLATE utf8_bin NOT NULL,
   `razon_social` varchar(30) COLLATE utf8_bin NOT NULL,
+  `rfc` varchar(10) COLLATE utf8_bin NOT NULL,
   `fecha_factura` date NOT NULL,
-  `domicilio` text COLLATE utf8_bin NOT NULL,
+  `direccion` text COLLATE utf8_bin NOT NULL,
+  `cp` int(5) NOT NULL,
+  `colonia` varchar(20) COLLATE utf8_bin NOT NULL,
+  `ciudad` varchar(20) COLLATE utf8_bin NOT NULL,
+  `estado` varchar(20) COLLATE utf8_bin NOT NULL,
+  `pais` varchar(20) COLLATE utf8_bin NOT NULL,
   `id_compra` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -99,6 +106,16 @@ CREATE TABLE `libros` (
   `editorial` varchar(20) COLLATE utf8_bin NOT NULL,
   `anio` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id_libro`, `nombre_libro`, `sinopsis`, `stock`, `cantidad_vend`, `genero`, `costo`, `editorial`, `anio`) VALUES
+(1, 'La Guerra de los Dioses El Sacrificio', 'Los tiempos son extraños en el imperio Mandrágora. Un dios de las cinco esencias fue asesinado, y nadie se explica cómo sucedió. Algunos dicen que el ', 60, 0, 'Fantasia', 30, 'Pablo Andrés Wunderl', 2016),
+(4, 'El enigma de la habitación 622', 'Joël Dicker Escritor suizo, Joël Dicker estudió Drama en París, pero volvió a Suiza donde estudió Derecho en la Universidad de Ginebra. Dicker ha desa', 34, 23, 'Novela', 239, 'Kindle', 2020),
+(6, '	 El Arte de la Guerra', 'El Arte de la Guerra es el mejor libro de estrategia de todos los tiempos. Inspiró a Napoleón, Maquiavelo, Mao Tse Tung y muchas más figuras histórica', 10, 2, 'Tratado', 69, 'Aegitas', 1869),
+(7, 'Sol de Medianoche', '¡Llega la nueva novela de Stephenie Meyer! ', 100, 89, 'Ficcion', 219, 'ALFAGUARA', 2020);
 
 --
 -- Índices para tablas volcadas
@@ -170,7 +187,7 @@ ALTER TABLE `facturas`
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
